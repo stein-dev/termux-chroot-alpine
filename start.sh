27e804962aaae62d274ef6a29b8ea3d23084e13e
stein-dev <pigzilla.012@gmail.com>
2022-02-07 19:10:38 +0800

    Initial commit

--- start.sh
+++ start.sh
@@ -33,7 +33,7 @@ export ANDROID_ART_ROOT=/apex/com.android.art
 export ANDROID_DATA=/data
 export ANDROID_I18N_ROOT=/apex/com.android.i18n
 export ANDROID_ROOT=/system
-export ANDROID_RUNTIME_ROOT=
+export ANDROID_RUNTIME_ROOT=/apex/com.android.runtime
 export ANDROID_TZDATA_ROOT=/apex/com.android.tzdata
 export BOOTCLASSPATH=/apex/com.android.art/javalib/core-oj.jar:/apex/com.android.art/javalib/core-libart.jar:/apex/com.android.art/javalib/core-icu4j.jar:/apex/com.android.art/javalib/okhttp.jar:/apex/com.android.art/javalib/bouncycastle.jar:/apex/com.android.art/javalib/apache-xml.jar:/system/framework/framework.jar:/system/framework/ext.jar:/system/framework/telephony-common.jar:/system/framework/voip-common.jar:/system/framework/ims-common.jar:/system/framework/framework-atb-backward-compatibility.jar:/system/framework/telephony-ext.jar:/system/framework/WfdCommon.jar:/apex/com.android.conscrypt/javalib/conscrypt.jar:/apex/com.android.media/javalib/updatable-media.jar:/apex/com.android.mediaprovider/javalib/framework-mediaprovider.jar:/apex/com.android.os.statsd/javalib/framework-statsd.jar:/apex/com.android.permission/javalib/framework-permission.jar:/apex/com.android.sdkext/javalib/framework-sdkextensions.jar:/apex/com.android.wifi/javalib/framework-wifi.jar:/apex/com.android.tethering/javalib/framework-tethering.jar
 export COLORTERM=truecolor
@@ -72,7 +72,7 @@ unset PREFIX
 
 echo "Done"
 
-busybox chroot $CHROOT /bin/sh --login
+busybox chroot $CHROOT /bin/bash --login
 
 echo "Unmounting $CHROOT/proc"
 busybox umount "$CHROOT/proc"
