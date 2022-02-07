7b3fdd294c24541b6f8886945fc1eb990c822d34
stein-dev <pigzilla.012@gmail.com>
2022-02-07 17:07:29 +0800

    Initial commit

---
+++ termux-proot.sh
@@ -0,0 +1,16 @@
+export ANDROID_ART_ROOT=/apex/com.android.art
+export ANDROID_DATA=/data
+export ANDROID_I18N_ROOT=/apex/com.android.i18n
+export ANDROID_ROOT=/system
+export ANDROID_RUNTIME_ROOT=
+export ANDROID_TZDATA_ROOT=/apex/com.android.tzdata
+export BOOTCLASSPATH=/apex/com.android.art/javalib/core-oj.jar:/apex/com.android.art/javalib/core-libart.jar:/apex/com.android.art/javalib/core-icu4j.jar:/apex/com.android.art/javalib/okhttp.jar:/apex/com.android.art/javalib/bouncycastle.jar:/apex/com.android.art/javalib/apache-xml.jar:/system/framework/framework.jar:/system/framework/ext.jar:/system/framework/telephony-common.jar:/system/framework/voip-common.jar:/system/framework/ims-common.jar:/system/framework/framework-atb-backward-compatibility.jar:/system/framework/telephony-ext.jar:/system/framework/WfdCommon.jar:/apex/com.android.conscrypt/javalib/conscrypt.jar:/apex/com.android.media/javalib/updatable-media.jar:/apex/com.android.mediaprovider/javalib/framework-mediaprovider.jar:/apex/com.android.os.statsd/javalib/framework-statsd.jar:/apex/com.android.permission/javalib/framework-permission.jar:/apex/com.android.sdkext/javalib/framework-sdkextensions.jar:/apex/com.android.wifi/javalib/framework-wifi.jar:/apex/com.android.tethering/javalib/framework-tethering.jar
+export COLORTERM=truecolor
+export DEX2OATBOOTCLASSPATH=/apex/com.android.art/javalib/core-oj.jar:/apex/com.android.art/javalib/core-libart.jar:/apex/com.android.art/javalib/core-icu4j.jar:/apex/com.android.art/javalib/okhttp.jar:/apex/com.android.art/javalib/bouncycastle.jar:/apex/com.android.art/javalib/apache-xml.jar:/system/framework/framework.jar:/system/framework/ext.jar:/system/framework/telephony-common.jar:/system/framework/voip-common.jar:/system/framework/ims-common.jar:/system/framework/framework-atb-backward-compatibility.jar:/system/framework/telephony-ext.jar:/system/framework/WfdCommon.jar
+export EXTERNAL_STORAGE=/sdcard
+[ -z "$LANG" ] && export LANG=C.UTF-8
+export PATH=${PATH}:/data/data/com.termux/files/usr/bin:/system/bin:/system/xbin
+export TERM=xterm-256color
+export TMPDIR=/tmp
+export PULSE_SERVER=127.0.0.1
+export MOZ_FAKE_NO_SANDBOX=1
